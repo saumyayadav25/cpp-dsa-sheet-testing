f1706883411c2c3fa70b7e23259331e2f4d55f5f
Shree@600 <shreejitabhawal@gmail.com>
2025-08-14 12:44:11 +0530

    Fallback empty string so that feom is never undefined in mail.ts file

--- lib/mail.ts
+++ lib/mail.ts
@@ -19,11 +19,13 @@ export const sendEmail = async ({
 }) => {
   try {
     await transporter.sendMail({
-      from: `"DSAMate" <${process.env.GOOGLE_APP_USER}>`,
+      from: process.env.GOOGLE_APP_USER || '',
+    
       to,
       subject,
       html,
     });
+    console.log("Email sent Successfully")
   
   } catch (error) {
     console.log("Failed to send email", error);
