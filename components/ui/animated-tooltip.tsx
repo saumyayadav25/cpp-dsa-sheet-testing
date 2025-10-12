26c3935a6fedd4c36e35d45e8236dc0f60448f85
MrunaliniPachpute <pachputemrunalini@gmail.com>
2025-10-12 22:25:34 +0530

    Updates testimonials to be visible in light mode

--- components/ui/animated-tooltip.tsx
+++ components/ui/animated-tooltip.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useState, useRef } from "react";
 import {
   motion,
@@ -26,11 +25,11 @@ export const AnimatedTooltip = ({
 
   const rotate = useSpring(
     useTransform(x, [-100, 100], [-45, 45]),
-    springConfig,
+    springConfig
   );
   const translateX = useSpring(
     useTransform(x, [-100, 100], [-50, 50]),
-    springConfig,
+    springConfig
   );
 
   const handleMouseMove = (event: any) => {
@@ -43,6 +42,8 @@ export const AnimatedTooltip = ({
       x.set(event.nativeEvent.offsetX - halfWidth);
     });
   };
+
+  
 
   return (
     <>
@@ -73,14 +74,15 @@ export const AnimatedTooltip = ({
                   rotate: rotate,
                   whiteSpace: "nowrap",
                 }}
-                className="absolute -top-16 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
+                className="absolute -top-16 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center justify-center rounded-md bg-white dark:bg-black px-4 py-2 text-xs shadow-xl"
               >
                 <div className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-gradient-to-r from-transparent via-emerald-500 to-transparent" />
+
                 <div className="absolute -bottom-px left-10 z-30 h-px w-[40%] bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
-                <div className="relative z-30 text-base font-bold text-white">
+                <div className="relative z-30 text-base font-bold text-black dark:text-white">
                   {item.name}
                 </div>
-                <div className="text-xs text-white">{item.designation}</div>
+                <div className="text-xs text-black dark:text-white">{item.designation}</div>
               </motion.div>
             )}
           </AnimatePresence>
